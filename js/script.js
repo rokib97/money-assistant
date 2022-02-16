255ca786857e4a4068554be0ef1790ed803a4373
rokib97 <hasanrokib1997@gmail.com>
2022-02-17 01:30:00 +0600

    finsihed the calculation

--- js/script.js
+++ js/script.js
@@ -13,18 +13,38 @@ function calculateExpenses() {
   const foodInputValue = getInputValue("food-input");
   const rentInputValue = getInputValue("rent-input");
   const clothesInputValue = getInputValue("clothes-input");
+  if (incomeInputValue < 0) {
+  }
 
   const totalExpenses =
     parseFloat(foodInputValue) +
     parseFloat(rentInputValue) +
     parseFloat(clothesInputValue);
 
+  // update total expenses and balance
   const previousTotalExpenses = document.getElementById("total-expenses");
   previousTotalExpenses.innerText = totalExpenses;
   const balance = document.getElementById("balance");
   balance.innerText = incomeInputValue - totalExpenses;
 }
+// function to calculate the saving
+function calculateSaving() {
+  const incomeInputValue = getInputValue("income-input");
+  //   const getPercentageValue = document.getElementById("percentage-input").value;
+  const getPercentageValue = getInputValue("percentage-input");
+  const previousSaving = document.getElementById("saving-amount");
+  previousSaving.innerText = (incomeInputValue * getPercentageValue) / 100;
+
+  //   update savings and remain balance
+  const balance = document.getElementById("balance");
+  const previousRemainingBalance = document.getElementById("previous-balance");
+  previousRemainingBalance.innerText =
+    balance.innerText - previousSaving.innerText;
+}
 // event handle for calculate button
 document.getElementById("calculate-btn").addEventListener("click", function () {
   calculateExpenses();
+});
+document.getElementById("save-btn").addEventListener("click", function () {
+  calculateSaving();
 });
